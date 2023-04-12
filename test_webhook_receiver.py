141dc476b6cf8f1a083b0822b449b4603aa434cc
gzapodea <gzapodea@cisco.com>
2023-04-12 10:12:29 -0700

    updated after code inspect

--- test_webhook_receiver.py
+++ test_webhook_receiver.py
@@ -28,15 +28,14 @@ __license__ = "Cisco Sample Code License, Version 1.1"
 import requests
 import json
 import urllib3
+import ssl
 
 from requests.auth import HTTPBasicAuth  # for Basic Auth
-
 from urllib3.exceptions import InsecureRequestWarning  # for insecure https warnings
 
 urllib3.disable_warnings(InsecureRequestWarning)  # disable insecure https warnings
 
 from config import WEBHOOK_URL, WEBHOOK_USERNAME, WEBHOOK_PASSWORD
-
 
 # set the payload for a Cisco DNA Center new event notification
 
@@ -89,13 +88,13 @@ dnac_param_resolved = {
     "timestamp": 1576122993751,
     "tags": "",
     "details": {
-      "Type": "Network Device",
-      "Assurance Issue Priority": "P1",
-      "Assurance Issue Details": "Interface GigabitEthernet0/0 on the following network device is down: Local Node: PDX-CORE2",
-      "Device": "10.93.141.3",
-      "Assurance Issue Name": "Interface GigabitEthernet0/0 is Down on Network Device 10.93.141.3",
-      "Assurance Issue Category": "Connectivity",
-      "Assurance Issue Status": "resolved"
+        "Type": "Network Device",
+        "Assurance Issue Priority": "P1",
+        "Assurance Issue Details": "Interface GigabitEthernet0/0 on the following network device is down: Local Node: PDX-CORE2",
+        "Device": "10.93.141.3",
+        "Assurance Issue Name": "Interface GigabitEthernet0/0 is Down on Network Device 10.93.141.3",
+        "Assurance Issue Category": "Connectivity",
+        "Assurance Issue Status": "resolved"
     },
     "ciscoDnaEventLink": "https://10.93.141.35/dna/assurance/issueDetails?issueId=4292b3bc-a8a7-4a2e-8349-ddfae1ea8b9e",
     "note": "To programmatically get more info see here - https://<ip-address>/dna/platform/app/consumer-portal/developer-toolkit/apis?apiId=8684-39bb-4e89-a6e4",
@@ -103,7 +102,6 @@ dnac_param_resolved = {
     "context": "",
     "tenantId": ""
 }
-
 
 # test the Webhook with a Cisco DNA Center sample notification
 
